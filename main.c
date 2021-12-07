4fdcc665cf100872a4b8904ed04a7d1dab3c0d5e
root <root@NELSON-G.localdomain>
2021-12-07 18:48:19 -0500

    main function of the program

---
+++ main.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * main - main function of the program
+ * @argc: number of parameters (not interective)
+ * @array: parameter array (not interective)
+ * Return: (0) success
+ */
+int main(_attribute_((unused))int argc, char *array[])
+{
+	size_t memory = 1;
+	ssize_t ending = 0;
+	char *comand = NULL, waiting[1024];
+	int i;
+
+	com_exit = 1;
+	if (isatty(fileno(stdin)))
+	{
+		signal(SIGINT, ctr_c);
+		while (com_exit != 0)
+		{
+			getcwd(waiting, sizeof(waiting));
+			i = _strlen(waiting);
+			waiting[i] = '$', waiting[i + 1] = ' ', waiting[i + 2] = '\0';
+			write(STDOUT_FILENO, waiting, _strlen(waiting));
+			ending = getline(&comand, &memory, stdin);
+			if (ending == -1)
+			{
+				free(comand);
+				write(STDOUT_FILENO, "\n", 1);
+				exit(127);
+			}
+			else if (*comand != 10)
+			{
+				select_command(comand, array[0]);
+				if (com_exit == 0)
+				{
+					free(comand);
+					return (0);
+				}
+			}
+			free(comand);
+			comand = NULL;
+		}
+	}
+	else
+		no_isatty(comand, memory, array);
+	free(comand);
+	return (0);
+}
