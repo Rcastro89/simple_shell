813fc291b6a6dcc072a831d9b2635646053971c7
Rcastro89 <r_castro_m@yahoo.es>
2021-12-08 17:58:24 -0500

    valgrind

--- main.h
+++ main.h
@@ -19,7 +19,7 @@
 typedef struct buily_in
 {
 	char *comand;
-	int (*f)(char **);
+	int (*f)(char *);
 } bu_in;
 
 
@@ -39,11 +39,11 @@ char *_strdup(char *str);
 void ctr_c(int ctr1_c);
 int only_comand(char *copycom, char *str1, char *comand, char *array, int ctr_error_isaty);
 void argv_exec(char *comand, char *exe, int ctr_error_isaty);
-int (exit_program(__attribute__((unused))char **command));
-int (*built_in(char *commad))(char **);
-int fun_error(__attribute__((unused))char **command);
+int (exit_program(__attribute__((unused))char *command));
+int (*built_in(char *commad))(char *);
+int fun_error(__attribute__((unused))char *command);
 int comp_comand_1(char *command, char simbol);
-int print_environ(__attribute__((unused))char **command);
+int print_environ(__attribute__((unused))char *command);
 void no_isatty(char *comand, size_t memory, char *array[]);
 
 #endif /* MAIN_H */
