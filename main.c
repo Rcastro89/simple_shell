f91f37e577c4b6fddb6b34bdd7cb9a367c14e297
Rcastro89 <r_castro_m@yahoo.es>
2021-12-08 18:41:06 -0500

    valgrind

--- main.c
+++ main.c
@@ -9,21 +9,22 @@ int main(__attribute__((unused))int argc, char *array[])
 {
 	size_t memory = 0;
 	ssize_t ending = 0;
-	char *comand = NULL, waiting[] = "prompt$ ";
+	char *comand, waiting[] = "prompt$ ";
 	int com_exit = 0;
 
-	if (isatty(fileno(stdin)))
-	{
-		signal(SIGINT, ctr_c);
+	
+	signal(SIGINT, ctr_c);
 		while (com_exit != 1)
 		{
-			write(STDOUT_FILENO, waiting, _strlen(waiting));
+			if (isatty(fileno(stdin)))
+				write(STDOUT_FILENO, waiting, _strlen(waiting));
 			comand = NULL;
 			ending = getline(&comand, &memory, stdin);
 			if (ending == -1)
 			{
 				free(comand);
-				write(STDOUT_FILENO, "\n", 1);
+				if (isatty(fileno(stdin)))
+					write(STDOUT_FILENO, "\n", 1);
 				exit(127);
 			}
 			else if (*comand != 10)
@@ -38,9 +39,6 @@ int main(__attribute__((unused))int argc, char *array[])
 			else
 				free(comand);
 		}
-	}
-	else
-		no_isatty(comand, memory, array);
 	free(comand);
 	return (0);
 }
